c0f9fd0c424fab5fa08c6453bcf804212741e2c6
Victor Garcia <vozzted@gmail.com>
2025-08-26 14:48:23 -0500

    Rama principal

---
+++ FUNAL S2/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FUNAL_S2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string name;
+            Console.WriteLine("CUAL ES TU NOMBRE");
+          name = Console.ReadLine();    
+            Console.WriteLine("TU NOMBRE ES "+ name);
+            Console.ReadKey();
+
+        }
+    }
+}
